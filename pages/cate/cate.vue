89972b6c18460cc6defb11d6a6232a0e7a998c23
lcruan <1416802331@qq.com>
2022-10-07 23:16:31 +0800

    完成首页

---
+++ pages/cate/cate.vue
@@ -0,0 +1,143 @@
+<template>
+  <view>
+    <view class="scroll-view-container">
+      <!-- 左侧滑动区域 -->
+      <scroll-view class="left-scroll-view" scroll-y="true" :style="{height: wh + 'px'}">
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx1</view>
+        <view class="left-scroll-view-item">xxx1</view>
+        <view class="left-scroll-view-item">xxx1</view>
+        <view class="left-scroll-view-item">xxx1</view>
+        <view class="left-scroll-view-item">xxx1</view>
+        <view class="left-scroll-view-item">xxx1</view>
+        <view class="left-scroll-view-item">xxx1</view>
+        <view class="left-scroll-view-item">xxx2</view>
+        <view class="left-scroll-view-item">xxx2</view>
+        <view class="left-scroll-view-item">xxx2</view>
+        <view class="left-scroll-view-item">xxx2</view>
+        <view class="left-scroll-view-item">xxx2</view>
+        <view class="left-scroll-view-item">xxx2</view>
+        <view class="left-scroll-view-item">xxx2</view>
+        <view class="left-scroll-view-item">xxx2</view>
+      </scroll-view>
+      <!-- 右侧滑动区域 -->
+      <scroll-view scroll-y="true" :style="{height: wh + 'px'}">
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy</view>
+        <view>yyy1</view>
+        <view>yyy1</view>
+        <view>yyy1</view>
+        <view>yyy1</view>
+        <view>yyy1</view>
+        <view>yyy1</view>
+        <view>yyy2</view>
+        <view>yyy2</view>
+        <view>yyy2</view>
+        <view>yyy2</view>
+        <view>yyy2</view>
+        <view>yyy2</view>
+        <view>yyy2</view>
+        <view>yyy2</view>
+        <view>yyy2</view>
+        <view>yyy2</view>
+        <view>yyy2</view>
+      </scroll-view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        // 当前设备可用的高度
+        wh: 0
+      };
+    },
+    onLoad() {
+      const sysInfo = uni.getSystemInfoSync()
+      this.wh = sysInfo.windowHeight
+    }
+  }
+</script>
+
+<style lang="scss">
+.scroll-view-container {
+  display: flex;
+  
+  .left-scroll-view {
+    width: 120px;
+    
+    .left-scroll-view-item {
+      background-color: #f7f7f7;
+      line-height: 60px;
+      text-align: center;
+      font-size: 12px;
+    }
+  }
+}
+</style>
